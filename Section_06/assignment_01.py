606d3f2d02d571ff7ce8ab0e6daf25c0cc702314
Ignas <ignas.ulozas@gmail.com>
2021-02-02 13:38:47 +0200

    Section 6 done. All done.

--- Section_06/assignment_01.py
+++ Section_06/assignment_01.py
@@ -1,32 +1,42 @@
-# Assignment 1:
+# Find the words specified in words_to_aggregate in dir_with_files. Find the number of times
+# The word appears in the files.
+# Expected output: the dictionary with the number of times the word was found total.
 
+import re
 import sys
 import os
-import re
 
-# directory_containing_files = "/Users/imtiazahmad/PycharmProjects/Assignments/Section_06/project_files" #sys.argv[1]
-# words_to_aggregate = ["hello", "Peter", "computer"] #sys.argv[2:]
-directory_containing_files = sys.argv[1]
+# Using hardcoded values.
+# dir_with_files = os.getcwd() + "\\project_files"
+# words_to_aggregate = ["hello", "Peter", "computer"]
+
+# Passing it out as a script in cmd.
+dir_with_files = sys.argv[1]
 words_to_aggregate = sys.argv[2:]
+results = {}
 
-# Expected Output:
-# {"there": n, "Michael": n, "running": n}
+for current_dir, dirs, files in os.walk(dir_with_files):
+    # We need only the file with the current dir.
+    for file in files:
+        # For readability.
+        print("The matches found in file:", file)
+        # Let's open the file in the directory.
+        with open(os.path.join(current_dir, file), encoding="utf-8") as f:
+            # Now let's check for the words in the file.
+            for word in words_to_aggregate:
+                # For readability. Also, we use a simple regex expression to find all the matches of a word.
+                found_item = re.findall(word, f.read())
+                # We get back a list, so we can just find the len of it.
+                # Plus additional print statements
+                print("The word:", word, "was found:", len(found_item), "times.")
+                # Add the word to the dict.
+                if word not in results:
+                    results[word] = 0
+                # If in the dict, just add the len of the list.
+                else:
+                    results[word] = results[word] + len(found_item)
+                # Come back to the start of the file for a second/third word, etc.
+                f.seek(0)
 
-words_and_counts = {}
-
-for word in words_to_aggregate:
-    count = 0
-    for path, folder_names, file_names in os.walk(directory_containing_files):
-        for file_name in file_names:
-            file = os.path.join(path, file_name)
-            with open(file, "r") as a_file:
-                for line in a_file:
-                    if re.search(word, line):
-                        word_list = re.findall(word, line)
-                        count += len(word_list)
-
-    words_and_counts[word] = count
-
-
-
-print(words_and_counts)
+# The expected output.
+print(results)
